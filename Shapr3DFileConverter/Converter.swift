3fb9b2c1f3f84f78e91e9de5f73ca2df34e22a30
Aldrich Co <aco@freelancer.com>
2020-07-11 18:42:30 +0800

    Created a fake converter

--- Shapr3DFileConverter/Converter.swift
+++ Shapr3DFileConverter/Converter.swift
@@ -8,18 +8,73 @@
 
 import Foundation
 
+public enum ConvertProgress {
+	case progress(_ progress: Float)
+	case completed(data: Data)
+	case error(message: String)
+}
+
 // takes a shapr file
 public class Converter {
 	
-	public init() {
+	public init() {}
+	
+	// asynchronously call progress
+	public func convert(_ input: Data, to type: FileExtension,
+						callback: @escaping ((ConvertProgress) -> Void)) {
 		
+		let queue = DispatchQueue(label: "conversion", qos: .userInitiated)
+		let total = 1000
+		
+		let progressBase = 120 // each tick adds a random progress to total,
+		// 1..progressBase
+		
+		// each tick, will compute a random delay (in tenths of a second)
+		// to use as the delay
+		let delayTenthsBase: Int = 4 // make it go from 1..base
+		
+		var counter = 0
+		var totalSecs = Double(0)
+		
+		queue.async { [weak self] in
+			while counter < total {
+				let delay = 1 + Int(arc4random()) % delayTenthsBase
+				totalSecs += Double(delay) / 10.0
+				counter += (1 + Int(arc4random()) % progressBase)
+				let nextCounter = counter
+				
+				self?.delay(bySeconds: totalSecs) {
+					
+					let value = Float(nextCounter) / Float(total)
+					
+					if value < 1 {
+						callback(.progress(value))
+					} else {
+						callback(.completed(data: Data()))
+					}
+				}
+			}
+		}
 	}
 	
-	public func foo() -> Int {
-		print("foo")
-		return 1
+	func delay(bySeconds seconds: Double,
+			   dispatchLevel: DispatchLevel = .main,
+			   closure: @escaping () -> Void) {
+		
+		let dispatchTime = DispatchTime.now() + seconds
+		dispatchLevel.dispatchQueue.asyncAfter(deadline: dispatchTime, execute: closure)
 	}
 	
-	// input: Data?
-	// ...
+	enum DispatchLevel {
+		case main, userInteractive, userInitiated, utility, background
+		var dispatchQueue: DispatchQueue {
+			switch self {
+			case .main:                 return DispatchQueue.main
+			case .userInteractive:      return DispatchQueue.global(qos: .userInteractive)
+			case .userInitiated:        return DispatchQueue.global(qos: .userInitiated)
+			case .utility:              return DispatchQueue.global(qos: .utility)
+			case .background:           return DispatchQueue.global(qos: .background)
+			}
+		}
+	}
 }
--- Shapr3DFileConverterTests/Shapr3DFileConverterTests.swift
+++ Shapr3DFileConverterTests/Shapr3DFileConverterTests.swift
@@ -11,15 +11,25 @@ import XCTest
 
 class Shapr3DFileConverterTests: XCTestCase {
 	
-	override func setUpWithError() throws {
-	}
+	override func setUpWithError() throws {}
 	
-	override func tearDownWithError() throws {
-	}
+	override func tearDownWithError() throws {}
 	
-	func testExample() throws {
-		let foo = Converter().foo()
-		XCTAssertEqual(foo, 1)
+	func testConvert() throws {
+		let expect = expectation(description: "should finish after some time")
+		let converter = Converter()
+		let data = Data() // not used in mock
+		
+		converter.convert(data, to: .obj) { progress in
+			print(progress)
+			switch progress {
+			case .completed: expect.fulfill()
+			default: break
+			}
+		}
+		
+		waitForExpectations(timeout: 10) { (error) in
+			print("done")
+		}
 	}
-	
 }
